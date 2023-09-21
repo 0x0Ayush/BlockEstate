02be707576947b2d03b62166f5e1fd0fdb4b93b4
RAGHAV4875 <90446098+RAGHAV4875@users.noreply.github.com>
2023-09-21 10:59:32 +0530

    Add files via upload

---
+++ img/index.js
@@ -0,0 +1,111 @@
+import collection from "./collection.png";
+import creatorbackground1 from "./creatorbackground-1.jpeg";
+import creatorbackground2 from "./creatorbackground-2.jpeg";
+import creatorbackground3 from "./IMG1.jpg";
+import creatorbackground4 from "./IMG2.jpg";
+import creatorbackground5 from "./IMG3.jpg";
+import creatorbackground6 from "./IMG4.jpg";
+import creatorbackground7 from "./IMG5.jpg";
+import creatorbackground8 from "./IMG6.jpg";
+import creatorbackground9 from "./IMG7.jpg";
+import creatorbackground10 from "./IMG8.jpg";
+import creatorbackground11 from "./IMG9.jpg";
+import user1 from "./MyImage.jpg";
+import user2 from "./user14.jpg";
+import user3 from "./user15.jpg";
+import user4 from "./user13.jpg";
+import user5 from "./user-5.png";
+import user6 from "./user-6.png";
+import user7 from "./user-7.png";
+import user_img from "./user_img.jpg";
+import user8 from "./user-8.png";
+import user9 from "./user-9.png";
+import user10 from "./user-10.png";
+import service1 from "./service-1.png";
+import service2 from "./service-2.png";
+import service3 from "./service-3.png";
+import service4 from "./service-4.png";
+import facebook from "./facebook.svg";
+import google from "./google.svg";
+import logo from "./logo.svg";
+import twitter from "./twitter.svg";
+import telegram from "./telegram.svg";
+import youtube from "./youtube.svg";
+import vimeo from "./vimeo.svg";
+import vector from "./VectorHIW.svg";
+import zgcmq from "./zgcmq.svg";
+import hero from "./IMG1.jpg";
+import nft_1 from "./nft_1.png";
+import nft_image_1 from "./IMG11.jpg";
+import nft_image_2 from "./IMG19.jpg";
+import nft_image_3 from "./IMG8.jpg";
+import musiceWave from "./musicwave.png";
+import update from "./update.png";
+import earn from "./eran.png";
+import hero2 from "./hero2.png";
+import upload from "./upload.png";
+import provider1 from "./provider-1.png";
+import provider2 from "./provider-2.png";
+import provider3 from "./provider-3.png";
+import provider4 from "./provider-4.png";
+import loader from "./loadergif.gif";
+import errorgif from "./error.gif";
+import transfer from "./transfer.gif";
+import transferEther from "./ethereTransfer.gif";
+
+export default {
+  hero2,
+  provider1,
+  provider2,
+  provider3,
+  provider4,
+  upload,
+  earn,
+  collection,
+  update,
+  musiceWave,
+  nft_image_1,
+  nft_image_2,
+  nft_image_3,
+  logo,
+  nft_1,
+  hero,
+  creatorbackground1,
+  creatorbackground2,
+  creatorbackground3,
+  creatorbackground4,
+  creatorbackground5,
+  creatorbackground6,
+  creatorbackground7,
+  creatorbackground8,
+  creatorbackground9,
+  creatorbackground10,
+  creatorbackground11,
+  user1,
+  user_img,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  user7,
+  user8,
+  user9,
+  user10,
+  service1,
+  service2,
+  service3,
+  service4,
+  facebook,
+  google,
+  telegram,
+  twitter,
+  vector,
+  vimeo,
+  youtube,
+  zgcmq,
+  loader,
+  errorgif,
+  transfer,
+  transferEther,
+};
